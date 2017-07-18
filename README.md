7770da5e4c1f037c44051c322b2ef7ca7dbaa154
xfsyMrFeng <xfsy_2015@163.com>
2017-07-18 16:28:24 +0800

    second commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# library
+工具集
+
+## 引入
+```
+https://jitpack.io/#fengwenyi/library/v20170718.1612.002
+```
+
+## 用法
+1. Utils类
+- toast方法
+```
+name: toast
+param: Context, String
+```
+2.AppManager类（管理Activity）
+- 实例化
+```
+ getInstance
+ ```
+
+- 添加Activity
+```
+name: addActivity
+param: Activity
+```
+
+- 获取当前Activity
+```
+name: getCurrentActivity
+param:
+```
+
+- 关闭当前Activity
+```
+name: finishCurrentActivity
+param:
+```
+
+- 关闭指定的Activity
+```
+name: finishActivity
+param: Activity
+```
+
+- 关闭所有Activity
+```
+name: finishAllActivity
+param:
+```
+
+- 退出APP
+```
+name: AppExit
+param: Context
+```
---
+++ fdsgfdgfdgdf/src/main/java/com/xfsy/library/tools/AppManager.java
@@ -0,0 +1,96 @@
+package com.xfsy.library.tools;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+
+import java.util.Stack;
+
+/**
+ * Name   : AppManager
+ * Desc   : ...
+ * Use    : ...
+ * Author : fengwenyi
+ * Tool   : Android studio
+ * Date   : 2017-07-18
+ * Time   : 15:22
+ */
+
+public class AppManager {
+
+    private static Stack<Activity> activityStack;
+    private static AppManager instance;
+
+    public AppManager() {
+    }
+
+    /**
+     * 单一实例
+     * @return AppManager
+     */
+    public static AppManager getInstance() {
+        if (null == instance) {
+            instance = new AppManager();
+        }
+        return instance;
+    }
+
+    /**
+     * 将 Activity 添加到堆栈中
+     * @param activity
+     */
+    public void addActivity(Activity activity) {
+        if (null == activityStack) {
+            activityStack = new Stack<>();
+        }
+        activityStack.add(activity);
+    }
+
+    /**
+     * 获取当前 Activity
+     * @return
+     */
+    public Activity getCurrentActivity() {
+        return activityStack.lastElement();
+    }
+
+    /**
+     * 关闭当前 Activity
+     */
+    public void finishCurrentActivity() {
+        Activity activity = activityStack.lastElement();
+        finishActivity(activity);
+    }
+
+    /**
+     * 关闭指定的 Activity
+     * @param activity
+     */
+    public void finishActivity(Activity activity) {
+        if (null != activity) {
+            activityStack.remove(activity);
+            activity.finish();
+            activity = null;
+        }
+    }
+
+    /**
+     * 关闭所有 Activity
+     */
+    public void finishAllActivity() {
+        for (int i = 0; i < activityStack.size(); i++) {
+            Activity activity = activityStack.get(i);
+            if (null != activity) {
+                activity.finish();
+            }
+        }
+        activityStack.clear();
+    }
+
+    public void AppExit(Context context) {
+        finishAllActivity();
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        activityManager.killBackgroundProcesses(context.getPackageName());
+        System.exit(0);
+    }
+}
